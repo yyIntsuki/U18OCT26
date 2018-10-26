5798a1a9225dd1bc8a4d4a9f9eb87c1379e22977
intsuki.yz <intsuki.yz@gmail.com>
2018-10-26 11:10:04 +0200

    Added Customer class and Notes

---
+++ U18OCT26/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U18OCT26
+{
+    class Customer
+    {
+        public string name { get; set; }
+        public int phoneNumber { get; set; }
+        public int customerID { get; set; }
+        public string email { get; set; }
+        public bool vipStatus { get; set; }
+        public string regDate { get; set; }
+    }
+}
